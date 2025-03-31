ed082680fc0a3c36395fe4aa30e935fa17a44aaf
mplik <biuro@mplik.pl>
2025-03-31 23:44:16 +0200

    Poprawki w service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,4 @@
 const CACHE_NAME = 'agent-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/service-worker.js',
-    '/images/MPLIK.PL sprzedaż Stripe Climate-1.png',
-    '/images/MPLIK.PL_sprzedaż_Stripe_Climate.png',
-    '/images/domek_81kb_500_500.png',
-    // Dodaj inne zasoby, które chcesz buforować
-];
-
 // Instalacja service workera
 self.addEventListener('install', event => {
     event.waitUntil(
