231307a8418121b865bd4dd2de06ffcdf65d1230
mplik <biuro@mplik.pl>
2025-03-31 22:39:19 +0200

    Dodano plik sw w projekcie agent oraz rejstracje sw w pliku index.html

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'agent-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/service-worker.js',
+    '/images/MPLIK.PL sprzedaż Stripe Climate-1.png',
+    '/images/MPLIK.PL_sprzedaż_Stripe_Climate.png',
+    '/images/domek_81kb_500_500.png',
+    // Dodaj inne zasoby, które chcesz buforować
+];
+
+// Instalacja service workera
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Aktywacja service workera
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Obsługa żądań sieciowych
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
