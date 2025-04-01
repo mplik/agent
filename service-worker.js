5d2f373401563e69821ced2d747827551c21783c
mplik <biuro@mplik.pl>
2025-04-01 22:17:26 +0200

    Zmodyfikowano plik service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,42 +9,44 @@ const urlsToCache = [
     '/images/flaga_7kb_250_250.png',        
 ];
 
-// Instalacja service workera
-self.addEventListener('install', event => {
+// Instalowanie service worker
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// Aktywacja service workera
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+      caches.open(CACHE_NAME)
+        .then((cache) => {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
         })
     );
-});
-
-// Obsługa żądań sieciowych
-self.addEventListener('fetch', event => {
+  });
+  
+  // Obsługa fetch
+  self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request).catch(() => {
+            return caches.match('/fallback.html'); // plik istniejacy w razie braku internetu
+          });
+        })
     );
-});
+  });
+  
+  // Aktualizacja service worker
+  self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
